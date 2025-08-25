69e5a5243a3b95dc6331fec648ff65b8661cb8c7
Juan Carlos Hurtado Cruz <53494291+juanhc1498@users.noreply.github.com>
2025-08-25 16:07:50 -0500

    upload: template landing page

---
+++ dummy28/src/menu-items/pages.tsx
@@ -0,0 +1,39 @@
+// assets
+import DollarOutlined from '@ant-design/icons/DollarOutlined';
+import LoginOutlined from '@ant-design/icons/LoginOutlined';
+import PhoneOutlined from '@ant-design/icons/PhoneOutlined';
+import RocketOutlined from '@ant-design/icons/RocketOutlined';
+import Dashboard from '@ant-design/icons/DashboardOutlined';
+import Study from '@ant-design/icons/BookOutlined';
+
+// type
+import { NavItemType } from 'types/menu';
+
+// icons
+const icons = { DollarOutlined, LoginOutlined, PhoneOutlined, RocketOutlined, Dashboard, Study };
+
+// ==============================|| MENU ITEMS - PAGES ||============================== //
+
+const pages: NavItemType = {
+  id: 'group-pages',
+  title: 'pages',
+  type: 'group',
+  children: [
+    {
+      id: 'dashboard',
+      title: 'Dashboard',
+      type: 'item',
+      url: '/dashboard',
+      icon: icons.Dashboard,
+    },
+    {
+      id: 'estudiosSuelo',
+      title: 'Estudios de Suelo',
+      type: 'item',
+      url: '/FloorStudies',
+      icon: icons.Study,
+    }
+  ]
+};
+
+export default pages;
