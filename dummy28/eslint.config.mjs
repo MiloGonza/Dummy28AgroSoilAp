91f03ff86f11e11e677562599f02a16b2aebec57
Camilo Gonzalez <milogonza_01@hotmail.com>
2025-09-01 20:48:28 -0500

    Eslint rules update

--- dummy28/eslint.config.mjs
+++ dummy28/eslint.config.mjs
@@ -60,6 +60,8 @@ const eslintConfig = [
       '@typescript-eslint/no-explicit-any': 'off',
       "@typescript-eslint/no-unused-vars": "off",
       '@typescript-eslint/no-unused-expressions': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
       'import/no-cycle': 'off',
       'prefer-destructuring': 'off',
       'import/no-extraneous-dependencies': 'off',
